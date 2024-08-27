75555975d90068c765c30bace6e39db67a49d55d
MalinovyjMakintosh <140470195+MalinovyjMakintosh@users.noreply.github.com>
2024-08-27 15:27:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,32 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors'); // Импортируйте cors
+const cors = require('cors');
 const User = require('./models/User');
+require('dotenv').config(); // Для использования переменных окружения
 
 const app = express();
 app.use(express.json());
-app.use(cors()); // Включите cors
+app.use(cors());
 
 // Подключение к базе данных MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const mongoURI = process.env.MONGO_URI; // Убедитесь, что MONGO_URI установлен в переменных окружения
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Could not connect to MongoDB...', err));
-
 
 // Маршрут для генерации реферальной ссылки
 app.post('/generate-referral', async (req, res) => {
     const { username } = req.body;
 
     try {
-        // Найти пользователя по имени
         let user = await User.findOne({ username });
 
-        // Если пользователь существует, вернуть его реферальную ссылку
         if (user) {
             return res.json({ referralLink: `https://t.me/your_bot_username?startapp=${user.referralLink}` });
         }
 
-        // Если пользователь не существует, создать нового с реферальной ссылкой
         const referralLink = `ref_${new mongoose.Types.ObjectId()}`;
         user = new User({ username, referralLink });
         await user.save();
@@ -49,7 +47,7 @@ app.post('/register', async (req, res) => {
 
         const referrer = await User.findOne({ referralLink: referralCode });
         if (referrer) {
-            referrer.referredCount += 1;
+            referrer.referredCount = (referrer.referredCount || 0) + 1;
             await referrer.save();
         }
 
@@ -62,6 +60,26 @@ app.post('/register', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+// Новый маршрут для обновления количества яблок
+app.post('/update-apples', async (req, res) => {
+    const { userId, appleCount } = req.body;
+
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        user.appleCount = appleCount;
+        await user.save();
+
+        res.status(200).json({ message: 'Apple count updated successfully' });
+    } catch (error) {
+        res.status(500).json({ error: 'Server error' });
+    }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
