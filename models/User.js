d5626cf9253ff14b00612204ed5a678246368859
MalinovyjMakintosh <140470195+MalinovyjMakintosh@users.noreply.github.com>
2024-09-02 18:12:32 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    referralLink: { type: String },
-    referredCount: { type: Number, default: 0 },
-    appleCount: { type: Number, default: 0 } // Поле для хранения количества яблок
+    telegramId: { type: String, required: true, unique: true },
+    referralLink: { type: String, unique: true },
+    referredCount: { type: Number, default: 0 }, // Количество приглашенных пользователей
+    apples: { type: Number, default: 0 } // Количество яблок
 });
 
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
